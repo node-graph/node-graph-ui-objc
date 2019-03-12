d343154df89cd5ecf561392639b568f5814d0506
Patrik Nyblad <patrik.nyblad@gmail.com>
2019-03-12 17:45:17 +0100

    Add inputs to input stack view

--- NodleUI/NodleViews/NodeView.m
+++ NodleUI/NodleViews/NodeView.m
@@ -23,17 +23,36 @@
 }
 
 - (void)setup {
-    
     self.titleLabel = [UILabel new];
     self.titleLabel.translatesAutoresizingMaskIntoConstraints = NO;
     self.titleLabel.textAlignment = NSTextAlignmentCenter;
     [self addSubview:self.titleLabel];
+    
+    self.inputsStackView = [UIStackView new];
+    self.inputsStackView.translatesAutoresizingMaskIntoConstraints = NO;
+    self.inputsStackView.axis = UILayoutConstraintAxisVertical;
+//    self.inputsStackView.directionalLayoutMargins
+    [self addSubview:self.inputsStackView];
+    
+    for (NodeInput *input in self.node.inputs) {
+        UILabel *inputLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 50, 20)];
+        inputLabel.text = input.key ?: @"no_key";
+        inputLabel.textColor = UIColor.whiteColor;
+        inputLabel.backgroundColor = [UIColor colorWithRed:1.0 green:1.0 blue:0.0 alpha:0.2];
+        [self.inputsStackView addArrangedSubview:inputLabel];
+    }
 }
 
 - (void)setupLayout {
+    // Title
     [[self.titleLabel.leadingAnchor constraintEqualToAnchor:self.leadingAnchor constant:Spacer] setActive:YES];
     [[self.titleLabel.trailingAnchor constraintEqualToAnchor:self.trailingAnchor constant:-Spacer] setActive:YES];
     [[self.titleLabel.topAnchor constraintEqualToAnchor:self.topAnchor constant:Spacer] setActive:YES];
+    
+    // Inputs
+    [[self.inputsStackView.topAnchor constraintEqualToAnchor:self.titleLabel.bottomAnchor constant:Spacer] setActive:YES];
+    [[self.inputsStackView.leadingAnchor constraintEqualToAnchor:self.leadingAnchor constant:Spacer] setActive:YES];
+    [[self.inputsStackView.widthAnchor constraintEqualToAnchor:self.widthAnchor multiplier:0.5] setActive:YES];
 }
 
 - (void)updateData {
