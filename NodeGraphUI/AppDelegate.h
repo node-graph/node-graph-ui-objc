64d58d5c3007cd3d3a5285a7109676c8ecb94e82
Patrik Nyblad <patrik.nyblad@gmail.com>
2019-09-02 22:51:53 +0200

    Rename NodleUI to NodeGraphUI

--- NodeGraphUI/AppDelegate.h
+++ NodeGraphUI/AppDelegate.h
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.h
-//  NodleUI
-//
-//  Created by Patrik Nyblad on 2019-02-01.
-//  Copyright © 2019 Nodle. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
@@ -14,4 +6,3 @@
 
 
 @end
-
